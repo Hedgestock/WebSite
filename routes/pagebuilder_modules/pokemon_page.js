57c8dda1e8afffb6a7c428c034461277194bad85
Hedgestock <pamarthur@free.fr>
2018-05-25 11:22:21 +0200

    Modified type-frame look
    Now the text and the background complies more with the actual pokemon
    type display chart.
    Melt of double type colors.

--- routes/pagebuilder_modules/pokemon_page.js
+++ routes/pagebuilder_modules/pokemon_page.js
@@ -1,9 +1,35 @@
 module.exports = {
+  typeColors: {
+    normal: "gainsboro",
+    fighting: "brown",
+    flying: "skyblue",
+    poison: "purple",
+    ground: "tan",
+    rock: "saddlebrown", // peru ?
+    ghost: "darkslateblue",
+    steel: "lightsteelblue",
+    fire: "orangered",
+    water: "royalblue",
+    grass: "limegreen", // forestgreen ?
+    electric: "yellow", // gold ?
+    psychic: "hotpink",
+    ice: "aliceblue",
+    dragon: "indigo",
+    dark: "#333333",
+    fairy: "orchid",
+    unknown: "grey",
+    shadow: "gray",
+  },
+  
   getTypes: function (pokemon) {
     var res = [{"name": "", "color": ""}, {"name": "", "color": ""}];
     res[0].name = pokemon.types[0].type.name;
+    res[0].color = this.typeColors[res[0].name];
     if(pokemon.types[1]) {
-      res[1].name = "/" + pokemon.types[1].type.name;
+      res[1].name = pokemon.types[1].type.name;
+      res[1].color = this.typeColors[res[1].name];
+    } else {
+      res[1].color = this.typeColors[res[0].name];
     }
     return res;
   },
@@ -15,5 +41,5 @@ module.exports = {
         return species.varieties[i];
     }
     console.log(new Error().stack);
-  }
+  },
 }
